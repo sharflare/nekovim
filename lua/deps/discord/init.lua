753050ec613e8721880cb762b208d4d80f1ff82c
shar <shar@noreply.codeberg.org>
2026-05-01 03:18:34 -0500

    feat: add macos support

--- lua/deps/discord/init.lua
+++ lua/deps/discord/init.lua
@@ -1,6 +1,6 @@
-require 'discord.uuid'
+require("discord.uuid")
 
-local struct = require 'discord.deps.struct'
+local struct = require("discord.deps.struct")
 local uv = vim.uv
 
 ---@class Discord
@@ -16,183 +16,204 @@ local Discord = {}
 ---@param logger    Logger
 ---@param listener? fun(opcode: number, msg: string)
 function Discord:setup(client_id, logger, listener)
-  if logger then self.logger = logger end
-  self.client_id = client_id
+	if logger then
+		self.logger = logger
+	end
+	self.client_id = client_id
 
-  self.pid = uv:os_getpid()
-  self.listeners = {}
+	self.pid = uv:os_getpid()
+	self.listeners = {}
 
-  if listener then
-    self.listeners[1] = listener
-  end
+	if listener then
+		self.listeners[1] = listener
+	end
 
-  self:test_sockets(function () self:authorize() end)
+	self:test_sockets(function()
+		self:authorize()
+	end)
 end
 
 function Discord:is_connected()
-  return self.pipe
+	return self.pipe
 end
 
 ---@private
 ---@return string[] sockets
 function Discord:get_sockets()
-  local files = {}
-  local sys = uv.os_uname().sysname
+	local files = {}
+	local sys = uv.os_uname().sysname
 
-  if sys == 'Linux' then
-    local dirs = {
-      vim.env.XDG_RUNTIME_DIR or '/tmp',
-      '/run/user/' .. uv.getuid()
-    }
+	if sys == "Linux" or sys == "Darwin" then
+		local dirs = {}
 
-    for _, dir in ipairs(dirs) do
-      local handle = uv.fs_scandir(dir)
+		if sys == "Linux" then
+			table.insert(dirs, vim.env.XDG_RUNTIME_DIR)
+			table.insert(dirs, "/run/user/" .. uv.getuid())
+		else -- Darwin
+			table.insert(dirs, vim.env.TMPDIR)
+		end
 
-      if not handle then
-        self.logger:error('Discord:get_sockets', 'Could not scan directory ('..dir..')')
-      else
-        while true do
-          local name, type = uv.fs_scandir_next(handle)
-          if not name then break end
+		for _, var in ipairs({ "TMP", "TEMP" }) do
+			local val = vim.env[var]
+			if val then
+				table.insert(dirs, val)
+			end
+		end
 
-          if type == 'socket' and name:match '^discord%-ipc%-' then
-            table.insert(files, dir..'/'..name)
-          end
-        end
-      end
-    end
+		table.insert(dirs, "/tmp")
 
-  elseif sys == 'Windows' then
-    local cmd = [[powershell -Command (Get-ChildItem \\.\pipe\).FullName | findstr discord]]
+		for _, dir in ipairs(dirs) do
+			if not dir then
+				goto continue
+			end
 
-    local f = assert(io.popen(cmd, 'r'))
-    local d = assert(f:read('*a'))
-    f:close()
+			local handle = uv.fs_scandir(dir)
+			if handle then
+				while true do
+					local name, ftype = uv.fs_scandir_next(handle)
+					if not name then
+						break
+					end
 
-    files = d:split '\n'
+					if ftype == "socket" and name:match("^discord%-ipc%-") then
+						table.insert(files, dir .. "/" .. name)
+					end
+				end
+			end
 
-  else
-    error('Unsupported system. Supported systems are Linux and Windows.')
-  end
+			::continue::
+		end
+	elseif sys == "Windows" then
+		local cmd = [[powershell -Command (Get-ChildItem \\.\pipe\).FullName | findstr discord]]
+		local f = assert(io.popen(cmd, "r"))
+		local d = assert(f:read("*a"))
+		f:close()
+		files = d:split("\n")
+	else
+		error("Unsupported system: " .. sys)
+	end
 
-  return files
+	return files
 end
 
 ---@private
 ---@param callback? fun(self: Discord)
 function Discord:test_sockets(callback)
-  local sockets = self:get_sockets()
+	local sockets = self:get_sockets()
 
-  self.logger:log('Discord:test_sockets', 'Trying to connected with '..tostring(#sockets)..' sockets')
+	self.logger:log("Discord:test_sockets", "Trying to connected with " .. tostring(#sockets) .. " sockets")
 
-  for i, socket in ipairs(sockets) do
-    local pipe = assert(uv.new_pipe(false))
-    local num = tostring(i)..'/'..tostring(#sockets)
+	for i, socket in ipairs(sockets) do
+		local pipe = assert(uv.new_pipe(false))
+		local num = tostring(i) .. "/" .. tostring(#sockets)
 
-    pipe:connect(socket, function (err)
-      if self:is_connected() then
-        pipe:close()
-        return
-      end
+		pipe:connect(socket, function(err)
+			if self:is_connected() then
+				pipe:close()
+				return
+			end
 
-      if err then
-        pipe:close()
-        self.logger:log('Discord:test_sockets', 'Could not connect with socket', num)
-        return
-      end
+			if err then
+				pipe:close()
+				self.logger:log("Discord:test_sockets", "Could not connect with socket", num)
+				return
+			end
 
-      self.pipe = pipe
-      self.pipe:read_start(function (err, chunk)
-        self:read(err, chunk)
-      end)
+			self.pipe = pipe
+			self.pipe:read_start(function(err, chunk)
+				self:read(err, chunk)
+			end)
 
-      self.logger:log('Discord:test_sockets', 'Successful connection with socket', socket, '('..num..')')
+			self.logger:log("Discord:test_sockets", "Successful connection with socket", socket, "(" .. num .. ")")
 
-      if callback then callback(self) end
-    end)
-  end
+			if callback then
+				callback(self)
+			end
+		end)
+	end
 end
 
 function Discord:disconnect()
-  if not self.pipe then self.pipe:shutdown() end
-
-  self.pipe:shutdown(function ()
-    if not self.pipe:is_active() then
-      self.pipe:close()
-    end
-    self.pipe = nil
-  end)
+	if not self.pipe then
+		return
+	end
+	self.pipe:shutdown(function()
+		if not self.pipe:is_active() then
+			self.pipe:close()
+		end
+		self.pipe = nil
+	end)
 end
 
 ---@param activity Presence
 function Discord:set_activity(activity)
-  local payload = {
-    cmd = 'SET_ACTIVITY',
-    nonce = Generate_uuid(),
-    args = {
-      activity = activity,
-      pid = self.pid
-    }
-  }
+	local payload = {
+		cmd = "SET_ACTIVITY",
+		nonce = Generate_uuid(),
+		args = {
+			activity = activity,
+			pid = self.pid,
+		},
+	}
 
-  self.logger:debug('Discord:set_activity', 'setting presence')
-  self:call(1, payload)
+	self.logger:debug("Discord:set_activity", "setting presence")
+	self:call(1, payload)
 end
 
 ---@private
 function Discord:authorize()
-  local payload = {
-    client_id = self.client_id,
-    v = 1
-  }
+	local payload = {
+		client_id = self.client_id,
+		v = 1,
+	}
 
-  self:call(0, payload)
+	self:call(0, payload)
 end
 
 ---@private
 ---@param err string|nil
 ---@param chunk? string
 function Discord:read(err, chunk)
-  if err then
-    self.logger:error('Discord:read', err)
-    return
-  elseif not chunk then
-    return
-  end
+	if err then
+		self.logger:error("Discord:read", err)
+		return
+	elseif not chunk then
+		return
+	end
 
-  chunk = tostring(chunk)
-  self.buf = (self.buf or '') .. chunk
+	chunk = tostring(chunk)
+	self.buf = (self.buf or "") .. chunk
 
-  while #self.buf >= 8 do
-    local opcode, len = struct.unpack('<ii', self.buf)
+	while #self.buf >= 8 do
+		local opcode, len = struct.unpack("<ii", self.buf)
 
-    if #self.buf < 8 + len then
-      break
-    end
+		if #self.buf < 8 + len then
+			break
+		end
 
-    local msg = self.buf:sub(9, 8 + len)
-    self.buf = self.buf:sub(9 + len)
+		local msg = self.buf:sub(9, 8 + len)
+		self.buf = self.buf:sub(9 + len)
 
-    for _, fn in ipairs(self.listeners) do
-      fn(opcode, msg)
-    end
-  end
+		for _, fn in ipairs(self.listeners) do
+			fn(opcode, msg)
+		end
+	end
 end
 
 ---@private
 ---@param opcode    number
 ---@param payload   table
 function Discord:call(opcode, payload)
-  vim.schedule(function ()
-    local body = vim.fn.json_encode(payload)
-    local msg = struct.pack('<ii', opcode, #body) .. body
+	vim.schedule(function()
+		local body = vim.fn.json_encode(payload)
+		local msg = struct.pack("<ii", opcode, #body) .. body
 
-    self.pipe:write(msg, function (err)
-      if err then
-        self.logger:error('Discord:call', 'writing:', err)
-      end
-    end)
-  end)
+		self.pipe:write(msg, function(err)
+			if err then
+				self.logger:error("Discord:call", "writing:", err)
+			end
+		end)
+	end)
 end
 
 return Discord
--- lua/deps/vim-sockets/init.lua
+++ lua/deps/vim-sockets/init.lua
@@ -1,6 +1,6 @@
-local msgpack = require 'vim-sockets.deps.msgpack'
+local msgpack = require("vim-sockets.deps.msgpack")
 
-require 'vim-sockets.lib.list_to_argv'
+require("vim-sockets.lib.list_to_argv")
 
 local uv = vim.loop
 
@@ -21,51 +21,57 @@ local VimSockets = {}
 ---@param logger      Logger
 ---@param vim_events? boolean
 function VimSockets:setup(dep_path, logger, vim_events)
-  self.socket = vim.v.servername
-  self.dep_path = dep_path or ''
-  self.logger = logger
-  self.sockets = {}
-  self.receivers = {}
-  self.os = { name = self.get_osname() }
+	self.socket = vim.v.servername
+	self.dep_path = dep_path or ""
+	self.logger = logger
+	self.sockets = {}
+	self.receivers = {}
+	self.os = { name = self.get_osname() }
 
-  self:register_self()
+	self:register_self()
 
-  if vim_events then
-    vim.api.nvim_create_user_command('PrintSockets', function() self:print_sockets() end, { nargs = 0 })
-    vim.api.nvim_create_user_command('PrintLogs', function() self.logger:print() end, { nargs = 0 })
-  end
+	if vim_events then
+		vim.api.nvim_create_user_command("PrintSockets", function()
+			self:print_sockets()
+		end, { nargs = 0 })
+		vim.api.nvim_create_user_command("PrintLogs", function()
+			self.logger:print()
+		end, { nargs = 0 })
+	end
 
-  vim.api.nvim_create_autocmd('VimLeavePre', {
-    callback = function() self:unregister_self() end
-  })
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		callback = function()
+			self:unregister_self()
+		end,
+	})
 end
 
----@return 'windows'|'linux'|'unkown' osname
+---@return 'windows'|'linux'|'macos'|'unknown' osname
 function VimSockets.get_osname()
-  local uname = uv.os_uname()
-
-  if uname.sysname:find('Windows') then
-    return 'windows'
-  elseif uname.sysname:find('Linux') then
-    return 'linux'
-  end
-
-  return 'unkown'
+	local uname = uv.os_uname()
+	if uname.sysname:find("Windows") then
+		return "windows"
+	elseif uname.sysname:find("Linux") then
+		return "linux"
+	elseif uname.sysname == "Darwin" then
+		return "macos"
+	end
+	return "unknown"
 end
 
 function VimSockets:print_sockets()
-  print(vim.fn.json_encode(self.sockets))
+	print(vim.fn.json_encode(self.sockets))
 end
 
 ---@param event     string
 ---@param data      any
 ---@param callback? fun(err: string|nil)
 function VimSockets:emit(event, data, callback)
-  self.logger:debug('emit', 'Emitting event', event, 'to', #self.sockets, 'sockets')
+	self.logger:debug("emit", "Emitting event", event, "to", #self.sockets, "sockets")
 
-  for _, socket in ipairs(self.sockets) do
-    self:emit_to(socket, event, data, callback)
-  end
+	for _, socket in ipairs(self.sockets) do
+		self:emit_to(socket, event, data, callback)
+	end
 end
 
 ---@param socket    string
@@ -73,96 +79,110 @@ end
 ---@param data      any
 ---@param callback? fun(err: string|nil)
 function VimSockets:emit_to(socket, event, data, callback)
-  ---@type ReceiverProps
-  local props = {
-    emitter = self.socket,
-    event = event,
-    data = data
-  }
+	---@type ReceiverProps
+	local props = {
+		emitter = self.socket,
+		event = event,
+		data = data,
+	}
 
-  self:call_remote_method(socket, 'receive_data', { props }, function(err)
-    if err then
-      self.logger:error('emit_to', 'Error emitting to', socket .. ':', err)
-    end
+	self:call_remote_method(socket, "receive_data", { props }, function(err)
+		if err then
+			self.logger:error("emit_to", "Error emitting to", socket .. ":", err)
+		end
 
-    if callback then callback(err) end
-  end)
+		if callback then
+			callback(err)
+		end
+	end)
 end
 
 ---@param event string
 ---@param fn    fun(props: ReceiverProps)
 function VimSockets:on(event, fn)
-  self.receivers[event] = fn
+	self.receivers[event] = fn
 end
 
 ---@private
 function VimSockets:register_self()
-  self.sockets = self:get_socket_paths()
+	self.sockets = self:get_socket_paths()
 
-  self.logger:debug('register_self', 'Registered self for', #self.sockets - 1, 'sockets')
+	self.logger:debug("register_self", "Registered self for", #self.sockets - 1, "sockets")
 
-  for i, socket in ipairs(self.sockets) do
-    if socket == self.socket then
-      self.sockets[i] = nil
-    else
-      self:call_remote_method(socket, 'register_socket', { self.socket }, function(err)
-        if err then
-          self.logger:error('register_self', 'Error registering for', socket .. ':', err)
-        end
-      end)
-    end
-  end
+	for i, socket in ipairs(self.sockets) do
+		if socket == self.socket then
+			self.sockets[i] = nil
+		else
+			self:call_remote_method(socket, "register_socket", { self.socket }, function(err)
+				if err then
+					self.logger:error("register_self", "Error registering for", socket .. ":", err)
+				end
+			end)
+		end
+	end
 end
 
 function VimSockets:unregister_self()
-  self.logger:debug('unregister_self', 'Unregistering self for', #self.sockets, 'sockets')
+	self.logger:debug("unregister_self", "Unregistering self for", #self.sockets, "sockets")
 
-  for _, socket in ipairs(self.sockets) do
-    self:call_remote_method(socket, 'unregister_socket', { self.socket }, function(err)
-      if err then
-        self.logger:error('unregister_self', 'Error unregistering for socket', socket .. ':', err)
-      end
-    end)
-  end
+	for _, socket in ipairs(self.sockets) do
+		self:call_remote_method(socket, "unregister_socket", { self.socket }, function(err)
+			if err then
+				self.logger:error("unregister_self", "Error unregistering for socket", socket .. ":", err)
+			end
+		end)
+	end
 end
 
 ---@private
 ---@return string[]
 function VimSockets:get_socket_paths()
-  local files = {}
+	local files = {}
 
-  if self.os.name == 'linux' then
-    local dirs = {
-      vim.env.XDG_RUNTIME_DIR or '/tmp',
-      '/run/user/'..uv.getuid()
-    }
+	if self.os.name == "linux" or self.os.name == "macos" then
+		local dirs = {}
 
-    for _, dir in ipairs(dirs) do
-      local handle = uv.fs_scandir(dir)
-      if not handle then
-        self.logger:error('Discord:get_sockets', 'Could not scan directory ('..dir..')')
-      else
-        while true do
-          local name, type = uv.fs_scandir_next(handle)
-          if not name then break end
+		if self.os.name == "linux" then
+			table.insert(dirs, vim.env.XDG_RUNTIME_DIR)
+			table.insert(dirs, "/run/user/" .. uv.getuid())
+		else -- macos
+			table.insert(dirs, vim.env.TMPDIR)
+		end
 
-          if type == 'socket' and name:match '^nvim%.' then
-            table.insert(files, dir..'/'..name)
-          end
-        end
-      end
-    end
-  elseif self.os.name == 'windows' then
-    local cmd = [[powershell -Command (Get-ChildItem \\.\pipe\).FullName | findstr nvim]]
+		table.insert(dirs, "/tmp")
 
-    local f = assert(io.popen(cmd))
-    local data = assert(f:read('*a'))
-    f:close()
+		for _, dir in ipairs(dirs) do
+			if not dir then
+				goto continue
+			end
 
-    files = data:split '\n'
-  end
+			local handle = uv.fs_scandir(dir)
+			if not handle then
+				self.logger:error("VimSockets:get_socket_paths", "Could not scan directory (" .. dir .. ")")
+			else
+				while true do
+					local name, ftype = uv.fs_scandir_next(handle)
+					if not name then
+						break
+					end
 
-  return files
+					if ftype == "socket" and name:match("^nvim%.") then
+						table.insert(files, dir .. "/" .. name)
+					end
+				end
+			end
+
+			::continue::
+		end
+	elseif self.os.name == "windows" then
+		local cmd = [[powershell -Command (Get-ChildItem \\.\pipe\).FullName | findstr nvim]]
+		local f = assert(io.popen(cmd))
+		local data = assert(f:read("*a"))
+		f:close()
+		files = data:split("\n")
+	end
+
+	return files
 end
 
 ---@private
@@ -171,12 +191,12 @@ end
 ---@param args      table
 ---@param callback? fun(err: string|nil)
 function VimSockets:call_remote_method(socket, name, args, callback)
-  local cmd_fmt = 'lua %s:%s(%s)'
+	local cmd_fmt = "lua %s:%s(%s)"
 
-  local arglist = ListToArgv(args)
-  local cmd = string.format(cmd_fmt, self.dep_path, name, arglist)
+	local arglist = ListToArgv(args)
+	local cmd = string.format(cmd_fmt, self.dep_path, name, arglist)
 
-  self:call_instance(socket, cmd, callback)
+	self:call_instance(socket, cmd, callback)
 end
 
 ---@private
@@ -184,13 +204,13 @@ end
 ---@param cmd       string
 ---@param callback? fun(err: string|nil)
 function VimSockets:call_instance(socket, cmd, callback)
-  local pipe = assert(uv.new_pipe(true))
+	local pipe = assert(uv.new_pipe(true))
 
-  pipe:connect(socket, function()
-    local packed = msgpack.pack({ 0, 0, 'nvim_command', { cmd } })
+	pipe:connect(socket, function()
+		local packed = msgpack.pack({ 0, 0, "nvim_command", { cmd } })
 
-    pipe:write(packed, callback)
-  end)
+		pipe:write(packed, callback)
+	end)
 end
 
 --- End client methods ---
@@ -199,27 +219,27 @@ end
 ---@private
 ---@param socket string
 function VimSockets:register_socket(socket)
-  self.logger:info('register_socket', 'Registering socket', socket)
-  table.insert(self.sockets, socket)
+	self.logger:info("register_socket", "Registering socket", socket)
+	table.insert(self.sockets, socket)
 end
 
 ---@private
 ---@param socket string
 function VimSockets:unregister_socket(socket)
-  self.logger:info('unregister_socket', 'Unregistering socket', socket)
-  self.sockets[socket] = nil
+	self.logger:info("unregister_socket", "Unregistering socket", socket)
+	self.sockets[socket] = nil
 end
 
 ---@private
 ---@param props ReceiverProps
 function VimSockets:receive_data(props)
-  self.logger:info('receive_data', 'Receiving event', props.event, 'from', props.emitter)
+	self.logger:info("receive_data", "Receiving event", props.event, "from", props.emitter)
 
-  local fn = self.receivers[props.event]
+	local fn = self.receivers[props.event]
 
-  if fn then
-    fn(props)
-  end
+	if fn then
+		fn(props)
+	end
 end
 
 return VimSockets
