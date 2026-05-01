f2dc75ecd273fc165b636bd6cb0508c49867c96d
shar <shar@noreply.codeberg.org>
2026-05-01 03:30:07 -0500

    fix: ephemeral buffer event fix

--- lua/nekovim/event_handlers.lua
+++ lua/nekovim/event_handlers.lua
@@ -1,4 +1,4 @@
-local Logger = require 'lib.log'
+local Logger = require("lib.log")
 
 ---@class EventHandlers
 ---@field private nekovim NekoVim
@@ -6,72 +6,90 @@ local EventHandlers = {}
 
 ---@param nekovim NekoVim
 function EventHandlers:setup(nekovim)
-  self.nekovim = nekovim
+	self.nekovim = nekovim
 
-  local events = {
-    ['VimLeavePre'] = function() self.nekovim:shutdown() end,
-    ['FocusGained'] = function() self.nekovim:update() end,
+	local events = {
+		["VimLeavePre"] = function()
+			self.nekovim:shutdown()
+		end,
+		["FocusGained"] = function()
+			self.nekovim:update()
+		end,
 
-    ---@param props {buf: integer}
-    ['BufEnter'] = function(props) self:handle_BufEnter(props) end,
-    ['BufWinEnter'] = function(props) self:handle_BufEnter(props) end,
-    ['WinEnter'] = function(props) self:handle_BufEnter(props) end,
+		---@param props {buf: integer}
+		["BufEnter"] = function(props)
+			self:handle_BufEnter(props)
+		end,
+		["BufWinEnter"] = function(props)
+			self:handle_BufEnter(props)
+		end,
+		["WinEnter"] = function(props)
+			self:handle_BufEnter(props)
+		end,
 
-    ---@param props {buf: integer}
-    -- ['ModeChanged'] = function(props) self:handle_ModeChanged(props) end,
+		---@param props {buf: integer}
+		-- ['ModeChanged'] = function(props) self:handle_ModeChanged(props) end,
 
-    ---@param props {buf: integer}
-    ['BufWipeout'] = function(props) self:handle_BufWipeout(props) end
-  }
+		---@param props {buf: integer}
+		["BufWipeout"] = function(props)
+			self:handle_BufWipeout(props)
+		end,
+	}
 
-  ---@param event string
-  local function trigger(event, props)
-    Logger:debug('EventHandlers:setup.trigger', event)
+	---@param event string
+	local function trigger(event, props)
+		Logger:debug("EventHandlers:setup.trigger", event)
 
-    vim.schedule(function()
-      self.nekovim:restart_idle_timer()
-      events[event](props)
-    end)
-  end
+		vim.schedule(function()
+			self.nekovim:restart_idle_timer()
+			events[event](props)
+		end)
+	end
 
-  for event in pairs(events) do
-    vim.api.nvim_create_autocmd(event, {
-      callback = function(props) trigger(event, props) end
-    })
-  end
+	for event in pairs(events) do
+		vim.api.nvim_create_autocmd(event, {
+			callback = function(props)
+				trigger(event, props)
+			end,
+		})
+	end
 end
 
 function EventHandlers:handle_ModeChanged(props)
-  local buf = self.nekovim.buffers_props[props.buf]
+	local buf = self.nekovim.buffers_props[props.buf]
 
-  -- When a folder is open for some reason the buffer is not
-  -- registered-it should be. For the moment I'll leave a check here.
+	-- When a folder is open for some reason the buffer is not
+	-- registered-it should be. For the moment I'll leave a check here.
 
-  if buf then
-    self.nekovim.buffers_props[props.buf].mode = vim.api.nvim_get_mode().mode
-    self.nekovim:update()
-  end
+	if buf then
+		self.nekovim.buffers_props[props.buf].mode = vim.api.nvim_get_mode().mode
+		self.nekovim:update()
+	end
 end
 
 function EventHandlers:handle_BufEnter(props)
-  -- TODO: 'Invalid buffer id: 1' error
-  local filePath = vim.api.nvim_buf_get_name(props.buf)
-  local fileType = vim.bo[props.buf].filetype or nil
-  print('BufEnter '..(filePath or 'nil')..' '..(fileType or 'nil'))
+	-- TODO: 'Invalid buffer id: 1' error
 
-  self.nekovim.current_buf = props.buf
-  self.nekovim:make_buf_props(props.buf)
-  self.nekovim:update()
+	if not vim.api.nvim_buf_is_valid(props.buf) then
+		return
+	end
+
+	local filePath = vim.api.nvim_buf_get_name(props.buf)
+	local fileType = vim.bo[props.buf].filetype or nil
+
+	self.nekovim.current_buf = props.buf
+	self.nekovim:make_buf_props(props.buf)
+	self.nekovim:update()
 end
 
 function EventHandlers:handle_BufWipeout(props)
-  -- Telescope creates temporary internal buffers that get destroyed using
-  -- BufWipeout after usage.
-  if not self.nekovim.buffers_props[props.buf] then
-    return
-  end
+	-- Telescope creates temporary internal buffers that get destroyed using
+	-- BufWipeout after usage.
+	if not self.nekovim.buffers_props[props.buf] then
+		return
+	end
 
-  self.nekovim.buffers_props[props.buf] = nil
+	self.nekovim.buffers_props[props.buf] = nil
 end
 
 return EventHandlers
